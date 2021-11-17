9700b6864894ababddcf65ab68ba39583feb3d9a
Akshita06 <68218824+akshita-k06@users.noreply.github.com>
2021-11-17 21:22:37 +0530

    Update CollatzSequence.java

--- CollatzSequence.java
+++ CollatzSequence.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CollatzSequence {
     public static void getCollatzSequence(int n){
+        int countOperations=0;
         while(n>1){
             System.out.print(n+ " ");
             if(n%2==0){
@@ -12,8 +13,9 @@ public class CollatzSequence {
             else{
                 n=3*n+1;
             }
+            countOperations++;
         }
-        System.out.print(1);
+        System.out.print(1+" "+countOperations);
     }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
